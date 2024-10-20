343d2dd075591f5aafedcef530b5fdfc051e15f5
Celina Bolanos <celinab23@gmail.com>
2024-10-20 12:54:42 +0200

    Add first function.

--- my_code.py
+++ my_code.py
@@ -1,2 +1,10 @@
-name = Celina
-last_name = Bolanos
+name = "Celina"
+last_name = "Bolanos"
+
+def name_len(name):
+    if len(name) < 5:
+        print('This is a short name.')
+    else:
+        print('This is not a short name.')
+
+name_len(name)
